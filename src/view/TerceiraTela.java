2ded1f81ffd1d4b1e841e584b79ec716d148541b
Matheus <matheus.garcez09@gmail.com>
2024-02-20 12:57:07 -0300

    criando TerceiraTela.java

---
+++ src/view/TerceiraTela.java
@@ -0,0 +1,32 @@
+package view;
+
+import javax.swing.*;
+import java.awt.Container;
+import java.text.ParseException;
+import javax.swing.text.MaskFormatter;
+public class TerceiraTela extends JFrame{
+    private JLabel lblRazaoS;
+    private JTextField txtRazaoS;
+    private JLabel lblRua;
+    private JTextField txtRua;
+    private JLabel lblBairro;
+    private JTextField txtBairro;
+    private JLabel lblCidade;
+    private JTextField txtCidade;
+    private JLabel lblUF;
+    private JTextField txtUF; // fazer como função de escolha do UF combo box
+    private JLabel lblCEP;
+    private JFormattedTextField txtCEP;
+    private JLabel lblCNPJ;
+    private JFormattedTextField txtCNPJ;
+    private JLabel lblCodigo;
+    private JFormattedTextField txtCodigo;
+    private JLabel lblNome;
+    private JTextField txtNome;
+    private JLabel lblContato;
+    private JTextField txtContato;
+    private JLabel lblFuncao;
+    private JTextField txtFuncao;
+
+
+}
